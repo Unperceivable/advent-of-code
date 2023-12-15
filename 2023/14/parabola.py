a6e8ca265699d4e22dca594cb0321ada2be19494
Unperceivable <unperceivable@pm.me>
2023-12-15 11:12:59 +0100

    solution to part 2 day 14 2023

--- 2023/14/parabola.py
+++ 2023/14/parabola.py
@@ -1,5 +1,5 @@
-# %% 
 """Solutions to https://adventofcode.com/2023/day/14 Puzzles."""
+import hashlib
 import re
 from pathlib import Path
 
@@ -9,9 +9,12 @@ class Parabola():
         self.control_panel = control_panel
         self.panel_height = len(self.control_panel)
         self.panel_width = len(self.control_panel[0])
-        self.spin_cycle = 1000000000
 
-    def north_total_load(self): 
+    def print_panel(self):
+        for row in self.control_panel:
+           print(row) 
+        print("XXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    def shift_north(self): 
         """Return total load on parabola if all rocks are north.
 
         O....#....
@@ -27,58 +30,106 @@ class Parabola():
 
         Move all rocks as north as possible where, O will roll, # stay in place. . are empty spaces
         Ca1culate the sum of all distances to the bottom of the controls for each rock, in this case 136"""
+
+        self.sort_cols(reverse=True)
+
+    def shift_east(self):
+       self.sort_rows() 
+
+    def shift_west(self):
+        self.sort_rows(reverse=True)
+
+    def shift_south(self):
+        self.sort_cols()
+
+    def sort_rows(self, reverse=False):
+        rows = []
+        for row in self.control_panel:
+            rock_pools = re.findall(r'[^#]+|#', row)
+            shifted_row = "".join(["".join(sorted(pool, reverse=reverse)) for pool in rock_pools])
+            rows.append(shifted_row)
             
+        self.set_control_panel(rows)
+    
+    def sort_cols(self, reverse=False):
+    
         pattern_height = len(self.control_panel)
         pattern_width = len(self.control_panel[0])
-        sum_of_loads = 0
+        cols = []
         for idx in range(pattern_width):
             col = [self.control_panel[row][idx] for row in range(pattern_height)]
             col = "".join(map(str, col))
         
-            stop_pattern = re.compile(r"#*")
-            stop_matches = stop_pattern.finditer(col)
-            stop_spans = [match.span() for match in stop_matches if match.group()] 
-            start = 0
-            rock_pools = [pool for pool in col.split("#") if pool]
-            idx_shift = 0
-            if stop_spans and stop_spans[0][0] == 0:
-                start = stop_spans[0][1]
-                idx_shift = 1
+            rock_pools = re.findall(r'[^#]+|#', col)
+            shifted_col = "".join(["".join(sorted(pool, reverse=reverse)) for pool in rock_pools])
+            cols.append(shifted_col)
             
-            col_str = {}
-            for idx, pool in enumerate(rock_pools):
-                pool_start = pattern_height-start
-                pool_str = pool.count("O")
-                col_str[pool_start] = pool_str
-                load = self.calc_load(pool_start, pool_str)
-                sum_of_loads += load
-                if  len(stop_spans) > idx+idx_shift :
-                    start = stop_spans[idx+idx_shift][1]
-        return sum_of_loads 
+        panel = self.rotate_panel(cols)
+        self.set_control_panel(panel)
 
+    def rotate_panel(self, panel):
+        rotated_panel = []
+        for row in panel:
+            for x, char in enumerate(row):
+                if x == len(rotated_panel):
+                    rotated_panel.append(char)
+                else:
+                    rotated_panel[x] = f"{rotated_panel[x]}{char}"
+        return rotated_panel
     def set_control_panel(self, settings):
         self.control_panel = settings
 
     def calc_north_load(self):
+        sum_north_load = 0
         for y, row in enumerate(self.control_panel):
             for x in row:
                 if x == "O":
-                    return self.panel_height - y
-                else:
-                    return 0
-        return
+                    sum_north_load += self.panel_height - y
+        return sum_north_load 
     
     def calc_load(self, x, n):
         return x*n - (n * (n - 1)) // 2
     
-     
-# %%
+    def get_hash(self, panel):
+        hash_object = hashlib.sha256(''.join(panel).encode())
+        hash_value = hash_object.hexdigest()[:8]
+        return hash_value
+
+    def spin_cycle(self, spin_cycle=1):
+        panel_memory = {}
+        cycle_memory = {}
+        for cycle in range(1, spin_cycle +1):
+            panel_memory_before = len(cycle_memory)
+            self.shift_north()
+            self.shift_west()
+            self.shift_south()
+            self.shift_east()
+            panel_after_cycle = self.control_panel
+            cycle_hash = self.get_hash(panel_after_cycle)
+            if cycle_hash not in cycle_memory.keys():
+                cycle_memory[cycle_hash] = cycle
+                panel_memory[cycle] = self.control_panel
+            panel_memory_after = len(cycle_memory)
+            
+            if panel_memory_before == panel_memory_after:
+                cycle_start = list(cycle_memory.keys()).index(cycle_hash)
+                cycle_length = (len(cycle_memory) - cycle_start)
+                last_cycle_offset = (spin_cycle - cycle_start) % cycle_length
+                cycle_idx = cycle_start + last_cycle_offset
+                last_cycle_panel = panel_memory[cycle_idx]
+                self.control_panel = last_cycle_panel
+                break
+
 if __name__ == "__main__":
     puzzle_input_path = Path("puzzle_input.txt")
     with open(puzzle_input_path) as puzzle_input_file:
 
         puzzle_input = puzzle_input_file.read().splitlines()
         parabola = Parabola(puzzle_input)
-        print(f"Solution to first problem: {parabola.north_total_load()}")
-        # print(f"Solution to second problem: {parabola.north_total_load(spin_cycle=True)}")
+        parabola.shift_north()
+        print(f"Solution to first problem: {parabola.calc_north_load()}")
+        
+        parabola = Parabola(puzzle_input)
+        parabola.spin_cycle(1000000000)
+        print(f"Solution to second problem: {parabola.calc_north_load()}")
         
--- 2023/14/tests/test_parabola.py
+++ 2023/14/tests/test_parabola.py
@@ -18,4 +18,109 @@ class TestHotSprings(unittest.TestCase):
                         "#OO..#....",]
 
         expected_result = 136
-        self.assertEqual(Parabola(puzzle_input).north_total_load(), expected_result)
+        
+        parabola = Parabola(puzzle_input)
+        parabola.shift_north()
+        self.assertEqual(parabola.calc_north_load(), expected_result)
+
+    def test_north_shift(self):
+        puzzle_input = ["O....#....",
+                        "O.OO#....#",
+                        ".....##...",
+                        "OO.#O....O",
+                        ".O.....O#.",
+                        "O.#..O.#.#",
+                        "..O..#O..O",
+                        ".......O..",
+                        "#....###..",
+                        "#OO..#....",]
+
+        north_shifted_result =["OOOO.#.O..",
+                    "OO..#....#",
+                    "OO..O##..O",
+                    "O..#.OO...",
+                    "........#.",
+                    "..#....#.#",
+                    "..O..#.O.O",
+                    "..O.......",
+                    "#....###..",
+                    "#....#....",]
+
+        parabola = Parabola(puzzle_input)
+        parabola.shift_north()
+        self.assertEqual(parabola.control_panel, north_shifted_result)
+
+    def test_spin_cycle(self):
+        puzzle_input = ["O....#....",
+                        "O.OO#....#",
+                        ".....##...",
+                        "OO.#O....O",
+                        ".O.....O#.",
+                        "O.#..O.#.#",
+                        "..O..#O..O",
+                        ".......O..",
+                        "#....###..",
+                        "#OO..#....",]
+        
+        parabola = Parabola(puzzle_input)
+        
+        first_cycle_result = [".....#....",
+            "....#...O#",
+            "...OO##...",
+            ".OO#......",
+            ".....OOO#.",
+            ".O#...O#.#",
+            "....O#....",
+            "......OOOO",
+            "#...O###..",
+            "#..OO#...."]
+
+        parabola.spin_cycle()
+        self.assertEqual(parabola.control_panel, first_cycle_result)
+        
+        second_cycle_result=[".....#....",
+            "....#...O#",
+            ".....##...",
+            "..O#......",
+            ".....OOO#.",
+            ".O#...O#.#",
+            "....O#...O",
+            ".......OOO",
+            "#..OO###..",
+            "#.OOO#...O"]
+        
+        parabola.spin_cycle()
+        self.assertEqual(parabola.control_panel, second_cycle_result)
+
+        third_cycle_result = [".....#....",
+            "....#...O#",
+            ".....##...",
+            "..O#......",
+            ".....OOO#.",
+            ".O#...O#.#",
+            "....O#...O",
+            ".......OOO",
+            "#...O###.O",
+            "#.OOO#...O"]
+        
+        parabola.spin_cycle()
+        self.assertEqual(parabola.control_panel, third_cycle_result)
+
+    def test_part_two(self):
+        """."""
+        puzzle_input = ["O....#....",
+                        "O.OO#....#",
+                        ".....##...",
+                        "OO.#O....O",
+                        ".O.....O#.",
+                        "O.#..O.#.#",
+                        "..O..#O..O",
+                        ".......O..",
+                        "#....###..",
+                        "#OO..#....",]
+        print("!!!!!!!!!!!!LAST TEST !!!!!!!!!!!!")
+        parabola = Parabola(puzzle_input)
+        parabola.spin_cycle(1000000000)
+        expected_result = 64
+        self.assertEqual(parabola.calc_north_load(), expected_result)
+        
