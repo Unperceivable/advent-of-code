7cd5fe8305993071029a3854f1d96bdd778c946f
Unperceivable <unperceivable@pm.me>
2024-01-12 21:06:22 +0100

    test part 1 day 14 year 2022

---
+++ 2022/14/tests/test_falling_sand.py
@@ -0,0 +1,15 @@
+import unittest
+from falling_sand import FallingSand
+class TestFallingSand(unittest.TestCase):
+
+    puzzle_input = ["498,4 -> 498,6 -> 496,6",
+                    "503,4 -> 502,4 -> 502,9 -> 494,9",]
+            
+    def test_part_one(self):
+        falling_sand = FallingSand(self.puzzle_input)
+        expected_result = 24
+        self.assertEqual(falling_sand.max_sand(), expected_result)
+
+    
+    def test_part_two(self):
+        pass
