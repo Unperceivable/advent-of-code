aa3073aa50ba99fe75d3c3821f4159f93a8185aa
Unperceivable <unperceivable@pm.me>
2024-01-14 14:10:58 +0100

    tests to part 1 of day 15

---
+++ 2022/15/tests/test_beacons.py
@@ -0,0 +1,27 @@
+import unittest
+from beacons import Beacons
+class TestBeaconsunittest.TestCase):
+
+    puzzle_input = ["Sensor at x=2, y=18: closest beacon is at x=-2, y=15",
+                    "Sensor at x=9, y=16: closest beacon is at x=10, y=16",
+                    "Sensor at x=13, y=2: closest beacon is at x=15, y=3",
+                    "Sensor at x=12, y=14: closest beacon is at x=10, y=16",
+                    "Sensor at x=10, y=20: closest beacon is at x=10, y=16",
+                    "Sensor at x=14, y=17: closest beacon is at x=10, y=16",
+                    "Sensor at x=8, y=7: closest beacon is at x=2, y=10",
+                    "Sensor at x=2, y=0: closest beacon is at x=2, y=10",
+                    "Sensor at x=0, y=11: closest beacon is at x=2, y=10",
+                    "Sensor at x=20, y=14: closest beacon is at x=25, y=17",
+                    "Sensor at x=17, y=20: closest beacon is at x=21, y=22",
+                    "Sensor at x=16, y=7: closest beacon is at x=15, y=3",
+                    "Sensor at x=14, y=3: closest beacon is at x=15, y=3",
+                    "Sensor at x=20, y=1: closest beacon is at x=15, y=3",]
+
+    def test_part_one(self):
+        beacons = Beacons(self.puzzle_input)
+        expected_result = 26
+        self.assertEqual(beacons.exclusion_zone()), expected_result)
+
+    
+    def test_part_two(self):
+        pass
