f7a4551ee3e053282e5c9afe5848efdf8267032e
Unperceivable <unperceivable@pm.me>
2024-01-17 20:10:17 +0100

    fixup! test to part 1 of day 18 year 2022

--- 2022/18/tests/test_boulder.py
+++ 2022/18/tests/test_boulder.py
@@ -2,7 +2,19 @@ import unittest
 from boulder import Boulder
 class TestBoulder(unittest.TestCase):
 
-    puzzle_input = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
+    puzzle_input = ["2,2,2",
+                    "1,2,2",
+                    "3,2,2",
+                    "2,1,2",
+                    "2,3,2",
+                    "2,2,1",
+                    "2,2,3",
+                    "2,2,4",
+                    "2,2,6",
+                    "1,2,5",
+                    "3,2,5",
+                    "2,1,5",
+                    "2,3,5",]
      
     def test_part_one(self):
         boulder = Boulder(self.puzzle_input)
