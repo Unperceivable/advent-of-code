6790918d52d9d7e60fb3d6eb5b0ba0b3cb753198
Unperceivable <unperceivable@pm.me>
2024-01-20 13:47:56 +0100

    solution to part 2 day 21 year 2022

--- 2022/21/monkey.py
+++ 2022/21/monkey.py
@@ -1,9 +1,7 @@
-# %%
 from pathlib import Path
-from collections import namedtuple
 import re
-
-Coord = namedtuple("Coord", ["x", "y", "z"])
+from sympy import symbols, sympify 
+from sympy.solvers import solve
 
 class Monkey():
     def __init__(self, puzzle_input: "list[str]"):
@@ -16,7 +14,6 @@ class Monkey():
             values[monkey]=f"({value})" 
         return values
     
-    
     def root_value(self):
         root_value = self.values["root"]
         variables  = self.get_variables(root_value)
@@ -27,12 +24,31 @@ class Monkey():
         root_value = eval(root_value)
         return int(root_value)
     
+    def human_value(self):
+        for val in ["-","+", "*", "/"]:
+            root_value = self.values["root"]
+            if val in root_value:
+                root_value = root_value.replace(val, "==")
+                break
+        variables  = self.get_variables(root_value)
+        while variables != ["humn"]:
+            for var in variables:
+                if var != "humn":
+                    root_value = root_value.replace(var, self.values[var])
+            variables = self.get_variables(root_value)
+        
+        humn = symbols("humn")
+        equality = sympify(root_value, evaluate=False)
+        human_value = solve(equality, humn)[0]
+        return int(human_value)
+
     def get_variables(self, value):
         str_pattern = re.compile(r"([a-z]+)")
         variables = str_pattern.findall(value)
         return variables
+
+
     
-# %%
 if __name__ == "__main__":
     puzzle_input_path = Path("puzzle_input.txt")
     with open(puzzle_input_path) as puzzle_input_file:
@@ -40,3 +56,5 @@ if __name__ == "__main__":
         puzzle_input = puzzle_input_file.read().splitlines()
         monkey = Monkey(puzzle_input)
         print(f"Solution to first problem: {monkey.root_value()}")
+        monkey = Monkey(puzzle_input)
+        print(f"Solution to second problem: {monkey.human_value()}")
