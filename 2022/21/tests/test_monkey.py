865577b569cda44cdabd452690926b5bb6842ffe
Unperceivable <unperceivable@pm.me>
2024-01-20 12:35:23 +0100

    fixup! test for part 1 day 21 year 2022

--- 2022/21/tests/test_monkey.py
+++ 2022/21/tests/test_monkey.py
@@ -2,18 +2,26 @@ import unittest
 from monkey import Monkey
 class TestMonkey(unittest.TestCase):
 
-    puzzle_input = ["1",
-                    "2",
-                    "-3",
-                    "3",
-                    "-2",
-                    "0",
-                    "4",]     
+    puzzle_input = ["root: pppw + sjmn",
+                    "dbpl: 5",
+                    "cczh: sllz + lgvd",
+                    "zczc: 2",
+                    "ptdq: humn - dvpt",
+                    "dvpt: 3",
+                    "lfqf: 4",
+                    "humn: 5",
+                    "ljgn: 2",
+                    "sjmn: drzm * dbpl",
+                    "sllz: 4",
+                    "pppw: cczh / lfqf",
+                    "lgvd: ljgn * ptdq",
+                    "drzm: hmdt - zczc",
+                    "hmdt: 32",]
 
     def test_part_one(self):
         monkey = Monkey(self.puzzle_input)
-        expected_result = 3
-        self.assertEqual(monkey.sum_positions(), expected_result)
+        expected_result = 152
+        self.assertEqual(monkey.root_value(), expected_result)
     
     def test_part_two(self):
         pass
