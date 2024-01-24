036e08645890fca759ceaeb448c24cad7fc23af5
Unperceivable <unperceivable@pm.me>
2024-01-24 21:32:27 +0100

    add test of day 25 year 2022

---
+++ 2022/25/tests/test_console.py
@@ -0,0 +1,25 @@
+import unittest
+from console import Console
+class TestConsole(unittest.TestCase):
+
+    puzzle_input = ["1=-0-2",
+                    "12111",
+                    "2=0=",
+                    "21",
+                    "2=01",
+                    "111",
+                    "20012",
+                    "112",
+                    "1=-1=",
+                    "1-12",
+                    "12",
+                    "1=",
+                    "122",]
+
+    def test_part_one(self):
+        console = Console(self.puzzle_input)
+        expected_result = "2=-1=0"
+        self.assertEqual(console.snafu_number(), expected_result)
+    
+    def test_part_two(self):
+        pass
