a53d8099f6254decbb3956ec7f621700ceba9cac
Unperceivable <unperceivable@pm.me>
2024-01-25 20:23:56 +0100

    commit solution to part 2 of day 1 year 2021

--- 2021/01/sonar.py
+++ 2021/01/sonar.py
@@ -3,12 +3,12 @@ import re
 
 class Sonar():
     def __init__(self, puzzle_input: "list[str]"):
-        self.count_larger_than_prev_value = self.count_larger_than_prev(puzzle_input)
+        self.values = puzzle_input
 
-    def count_larger_than_prev(self, values):
-        values = [int(int(values[i - 1]) < int(x)) for i, x in enumerate(values) if i > 0]
-        return sum(values)
-    
+    def count_larger_than_prev(self, array_size=1):
+        sum_of_values = [sum([int(v) for v in self.values[i-array_size:i]]) for i in range(array_size,len(self.values)+1,1)]
+        values_larger_than_prev = [int(int(sum_of_values[i - 1]) < int(x)) for i, x in enumerate(sum_of_values) if i > 0]
+        return sum(values_larger_than_prev)
         
 if __name__ == "__main__":
     puzzle_input_path = Path("puzzle_input.txt")
@@ -16,4 +16,5 @@ if __name__ == "__main__":
 
         puzzle_input = puzzle_input_file.read().splitlines()
         sonar = Sonar(puzzle_input)
-        print(f"Solution to first problem: {sonar.count_larger_than_prev_value}")
+        print(f"Solution to first problem: {sonar.count_larger_than_prev()}")
+        print(f"Solution to second problem: {sonar.count_larger_than_prev(array_size=3)}")
