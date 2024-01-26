4ffe63c56cae5f01a6d397958b3e3fb8bb390c6c
Unperceivable <unperceivable@pm.me>
2024-01-26 19:08:27 +0100

    add test to part 1 of day 2 year 2021

---
+++ 2021/02/tests/test_dive.py
@@ -0,0 +1,19 @@
+import unittest
+from dive import Dive
+class TestDive(unittest.TestCase):
+
+    puzzle_input = ["forward 5",
+                    "down 5",
+                    "forward 8",
+                    "up 3",
+                    "down 8",
+                    "forward 2",]
+
+
+    def test_part_one(self):
+        dive = Dive(self.puzzle_input)
+        expected_result = 150
+        self.assertEqual(dive.get_dive_length(), expected_result)
+    
+    def test_part_two(self):
+        pass
