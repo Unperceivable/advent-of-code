74cae4bb69a34b49da9804db4756e5e135666ad8
Unperceivable <unperceivable@pm.me>
2024-01-29 20:25:49 +0100

    add tests to part 1 day 5 year 2021

---
+++ 2021/05/tests/test_vents.py
@@ -0,0 +1,22 @@
+import unittest
+from vents import Vents
+class TestVents(unittest.TestCase):
+
+    puzzle_input = ["0,9 -> 5,9",
+                    "8,0 -> 0,8",
+                    "9,4 -> 3,4",
+                    "2,2 -> 2,1",
+                    "7,0 -> 7,4",
+                    "6,4 -> 2,0",
+                    "0,9 -> 2,9",
+                    "3,4 -> 1,4",
+                    "0,0 -> 8,8",
+                    "5,5 -> 8,2",]
+
+    def test_part_one(self):
+        vents = Vents(self.puzzle_input)
+        expected_result = 5
+        self.assertEqual(vents.num_overlapping_lines(), expected_result)
+    
+    def test_part_two(self):
+        pass
