96b71f3ce02a884d16eb8b4303956d8c2c40634f
Unperceivable <unperceivable@pm.me>
2024-01-29 22:05:24 +0100

    add solution to part 2 of day 5 year 2021

--- 2021/05/vents.py
+++ 2021/05/vents.py
@@ -24,15 +24,14 @@ class Vents():
 
             
 
-    def num_overlapping_lines(self):
-        
+    def num_overlapping_lines(self, diagonal_lines=False):
         
         sea_floor = np.zeros(shape=(self.max_x+1, self.max_y+1))        
         for vent_line in self.vent_lines:
-            diff_x = bool(vent_line.end.x - vent_line.start.x)
-            diff_y = bool(vent_line.end.y - vent_line.start.y)
+            diff_x = int(vent_line.end.x - vent_line.start.x)
+            diff_y = int(vent_line.end.y - vent_line.start.y)
 
-            if diff_x ^ diff_y:
+            if bool(diff_x) ^ bool(diff_y):
                 if diff_x:
                     x_values =[vent_line.start.x, vent_line.end.x]
                     for x in range(min(x_values), max(x_values)+1):
@@ -41,6 +40,21 @@ class Vents():
                     y_values = [vent_line.start.y, vent_line.end.y]
                     for y in range(min(y_values), max(y_values)+1):
                         sea_floor[y, vent_line.start.x] += 1
+                
+            elif diagonal_lines:
+                step_x = int(diff_x/abs(diff_x))
+                step_y = int(diff_y/abs(diff_y))
+            
+                x_values = range(vent_line.start.x, vent_line.end.x + step_x, step_x)
+                y_values = range(vent_line.start.y, vent_line.end.y + step_y, step_y)
+
+                print(vent_line)
+                for x,y in zip(x_values, y_values):
+                    print((x,y))
+                    sea_floor[y, x] += 1
+
+        print(sea_floor)
+        print("-----------------")
         return np.sum(sea_floor > 1)
 
 if __name__ == "__main__":
@@ -50,3 +64,4 @@ if __name__ == "__main__":
         puzzle_input = puzzle_input_file.read().splitlines()
         vents = Vents(puzzle_input)
         print(f"Solution to first problem: {vents.num_overlapping_lines()}")
+        print(f"Solution to second problem: {vents.num_overlapping_lines(diagonal_lines=True)}")
